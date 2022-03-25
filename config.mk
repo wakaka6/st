433d08201cb6e2d85060bfc525192939e7526f3a
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2022-03-25 18:07:06 +0800

    first build

--- config.mk
+++ config.mk
@@ -7,8 +7,8 @@ VERSION = 0.8.5
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/include/X11
 
 PKG_CONFIG = pkg-config
 
@@ -22,7 +22,7 @@ LIBS = -L$(X11LIB) -lm -lrt -lX11 -lutil -lXft \
 
 # flags
 STCPPFLAGS = -DVERSION=\"$(VERSION)\" -D_XOPEN_SOURCE=600
-STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS)
+STCFLAGS = $(INCS) $(STCPPFLAGS) $(CPPFLAGS) $(CFLAGS) -O3 -fomit-frame-pointer
 STLDFLAGS = $(LIBS) $(LDFLAGS)
 
 # OpenBSD:
